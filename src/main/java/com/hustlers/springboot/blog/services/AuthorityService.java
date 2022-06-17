640b09b9adbbc5078d7b4028ef517b7e882b1de5
fanglu1 <101123629+fanglu1@users.noreply.github.com>
2022-06-17 09:01:07 -0400

    Update AuthorityService.java

--- src/main/java/com/hustlers/springboot/blog/services/AuthorityService.java
+++ src/main/java/com/hustlers/springboot/blog/services/AuthorityService.java
@@ -1,12 +1,10 @@
 package com.hustlers.springboot.blog.services;
 
 import com.hustlers.springboot.blog.model.Authority;
-
 import java.util.Set;
 
 public interface AuthorityService {
 
     Set<Authority> findAll();
-
     Set<Authority> findByUsername(String username);
 }
