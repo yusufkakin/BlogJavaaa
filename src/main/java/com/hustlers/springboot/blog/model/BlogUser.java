67546714cd700658e826141199f87776911b3a8a
fanglu1 <101123629+fanglu1@users.noreply.github.com>
2022-06-17 08:52:55 -0400

    Update BlogUser.java

--- src/main/java/com/hustlers/springboot/blog/model/BlogUser.java
+++ src/main/java/com/hustlers/springboot/blog/model/BlogUser.java
@@ -49,19 +49,19 @@ public class BlogUser {
 
 //    Error message will pop us stating that the Full Name, Username, and Password must be a certain length
     @NotNull(message = "is required")
-    @Size(min = 4, max = 30, message = "Username size should be in range [4...30]")
+    @Size(min = 4, max = 30, message = "Username should be a minimum of 4 characters")
     private String username;
 
     @NotNull(message = "is required")
-    @Size(min = 8, max = 50,message = "Password minimum of 8 character")
+    @Size(min = 8, max = 50,message = "Password should be minimum of 8 character")
     private String password;
 
     @NotNull(message = "is required")
-    @Size(min = 8, max = 50, message = "Password minimum of 8 character")
+    @Size(min = 8, max = 50, message = "Password should be minimum of 8 character")
     private String matchingPassword;
 
     @NotNull(message = "is required")
-    @Size(min = 4, max = 50, message = "Full Name size should be in range [4...50]")
+    @Size(min = 4, max = 50, message = "Full Name should be minimum of 4 characters")
     private String fullName;
 
 
